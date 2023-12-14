7031ebfcc3f936e4c94e76fbe192248a31e2e0ea
Libor Wagner (wagnelib) <libor.wagner@cvut.cz>
2023-12-14 06:51:14 +0100

    Recursive solusution for day12 too slow

--- aoc_2023_rust/day12/src/main.rs
+++ aoc_2023_rust/day12/src/main.rs
@@ -1,7 +1,9 @@
-use std::{str::FromStr, fmt::Display, collections::HashMap};
+use std::{str::FromStr, fmt::Display, collections::HashMap, iter::once};
 
+use itertools::Itertools;
 use rayon::prelude::*;
 use took::took;
+use memoize::memoize;
 
 macro_rules! aoc_task {
     ($f:expr) => {
@@ -31,31 +33,55 @@ fn parse(input: &str) -> Vec<(&str, Vec<usize>)> {
 type Cache = HashMap<(usize, usize, usize), usize>;
 
 
-fn ways(s: &[u8], check: &[usize], cur: Option<usize) -> usize {
+#[memoize]
+fn ways(s: &[u8], check: &[usize], cur: Option<usize>) -> usize {
 
-    match s[0] {
-        b'.' => ways(&s[1..], check, 0),
-        b'#' => ways(&s[1..], check, cur + 1),
-        b'?' => {
-            let a = [b'#'].iter().chain(s[1..].iter()).collect();
-            let b = [b'.'].iter().chain(s[1..].iter()).collect();
-            ways(&a, check, cur) + ways(&b, check, cur)
+    if s.is_empty() {
+        return match (cur, check.len()) {
+            (None, 0) => 1,
+            (Some(x), 1) if x == check[0] => 1,
+            _ => 0
         }
     }
 
+    if cur.is_some() && check.is_empty() {
+        return 0;
+    }
 
+    match (s[0], cur) {
+        (b'.', Some(x)) if x != check[0] => 0,
+        (b'.', Some(_)) => ways(&s[1..], &check[1..], None),
+        (b'.', None)    => ways(&s[1..], check, None),
+        (b'#', Some(x)) => ways(&s[1..], check, Some(x+1)),
+        (b'#', None)    => ways(&s[1..], check, Some(1)),
+        (b'?', Some(x)) => {
+            let ans = ways(&s[1..], check, Some(x+1));
+            if x == check[0] {
+                ans + ways(&s[1..], &check[1..], None) // has .
+            } else {
+                ans
+            }
+        },
+        (b'?', None) => ways(&s[1..], check, Some(1)) + ways(&s[1..], check, None),
+        _ => unreachable!()
+    }
 }
 
 
 fn part1(input: &str) -> usize {
     parse(input).into_par_iter().map(|(s, check)| {
-        let mut cache = Cache::new();
-        ways(s.as_bytes(), &check, 0)
+        ways(s.as_bytes(), &check, None)
     }).sum()
 }
 
 fn part2(input: &str) -> usize {
-    1
+    parse(input).into_par_iter().map(|(s, check)| {
+        let s = once(s).cycle().take(5).join("?");
+        let check = once(check).cycle().take(5).flatten().collect::<Vec<usize>>();
+        let n = ways(s.as_bytes(), &check, None);
+        println!("{s}, {check:?} => {n}");
+        n
+    }).sum()
 }
 
 #[cfg(test)]
@@ -79,15 +105,15 @@ mod tests {
         assert_eq!(part1(input), 7118);
     }
 
-    //#[test]
-    //fn part2_test() {
-    //    let input = include_str!("../input_test.txt");
-    //    assert_eq!(part2(input), 525152);
-    //}
+    #[test]
+    fn part2_test() {
+        let input = include_str!("../input_test.txt");
+        assert_eq!(part2(input), 525152);
+    }
 
-    //#[test]
-    //fn part2_final_test() {
-    //    let input = include_str!("../input.txt");
-    //    assert_eq!(part2(input), 1);
-    //}
+    #[test]
+    fn part2_final_test() {
+        let input = include_str!("../input.txt");
+        assert_eq!(part2(input), 1);
+    }
 }

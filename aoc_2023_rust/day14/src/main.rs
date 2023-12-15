cf8915b9d183c49c8920c69aef258522b6a7d0a9
Libor Wagner (wagnelib) <libor.wagner@cvut.cz>
2023-12-15 17:38:09 +0100

    Finished day14

--- aoc_2023_rust/day14/src/main.rs
+++ aoc_2023_rust/day14/src/main.rs
@@ -1,8 +1,11 @@
 use std::collections::HashMap;
+use std::fmt::Debug;
+use std::hash::Hash;
 
 use took::took;
 use common::map::*;
 use common::pos::*;
+use itertools::Itertools;
 
 macro_rules! aoc_task {
     ($f:expr) => {
@@ -17,9 +20,36 @@ fn main() {
     aoc_task!(|| part2(input));
 }
 
+
+pub fn repeat<T: Debug, K: Eq + Hash>(
+    mut state: T,
+    mut next: impl FnMut(T) -> T,
+    mut key_fn: impl FnMut(&T) -> K,
+    n: usize
+) -> T {
+    let mut hist: HashMap<K, usize> = HashMap::new();
+
+    for i in 0..n {
+        let key = key_fn(&state);
+        if let Some(offset) = hist.get(&key) {
+            let period = i - offset;
+            let n_left = (n - i) % period;
+            for _ in 0..n_left {
+                state = next(state);
+            }
+            return state;
+        } else {
+            hist.insert(key, i);
+            state = next(state);
+        }
+    }
+    state
+}
+
+
 fn parse(input: &str) -> SparseMap<char>  {
-    input.lines().enumerate().map(|(x,l)| {
-        l.chars().enumerate().filter_map(move |(y,c)| {
+    input.lines().enumerate().map(|(x, l)| {
+        l.chars().enumerate().filter_map(move |(y, c)| {
             if c != '.' {
                 Some(((x,y).into(), c))
             } else {
@@ -28,6 +58,8 @@ fn parse(input: &str) -> SparseMap<char>  {
         })
     }).flatten().collect()
 }
+
+
 
 #[derive(Debug)]
 enum Direction {
@@ -88,14 +120,14 @@ fn roll(map: &mut SparseMap<char>, dir: &Direction) -> () {
         Direction::East => {
             for x in 0..=bb.x_max {
                 first_empty = None;
-                for y in 0..=bb.y_max {
+                for y in (0..=bb.y_max).rev() {
                     let key = (x, y).into();
                     match (map.get(&key), first_empty) {
                         (Some('O'), None) => (),
                         (Some('O'), Some(p)) => {
                             map.remove(&key);
                             map.insert(p, 'O');
-                            first_empty = first_empty.map(|p| p + (0, 1).into());
+                            first_empty = first_empty.map(|p| p + (0, -1).into());
                         },
                         (None, None) => first_empty = Some(key),
                         (None, _) => (),
@@ -109,7 +141,7 @@ fn roll(map: &mut SparseMap<char>, dir: &Direction) -> () {
         Direction::West => {
             for x in 0..=bb.x_max {
                 first_empty = None;
-                for y in (0..=bb.y_max).rev() {
+                for y in 0..=bb.y_max {
                     let key = (x, y).into();
                     match (map.get(&key), first_empty) {
                         (Some('O'), None) => (),
@@ -146,36 +178,51 @@ fn part1(input: &str) -> isize {
     load(&map)
 }
 
-fn hash(map: &SparseMap<char>) -> String {
-
-    map.iter().filter_map(|k, c| {
-        if
-    }).sorted
-
-
+fn map_state_key(map: &SparseMap<char>) -> String {
+    let mut hash = String::new();
+    map.iter()
+       .filter_map(|(p, &c)| {
+           if c == 'O' {
+               Some(p)
+           } else {
+               None
+           }
+       }).sorted().for_each(|p| {
+           hash.push_str(format!("{}-{},", p.x, p.y).as_str());
+       });
+    hash
 }
 
 fn part2(input: &str) -> isize {
-    let hist: HashMap<String, usize> = HashMap::new();
     let mut map = parse(input);
-    [Direction::North, Direction::West, Direction::South, Direction::East].iter()
-        .cycle().take(1000000000*4).enumerate().find(|(j, dir)| {
-            roll(&mut map, dir);
-            let key = hash(&map);
-            if let Some(i) = hist.get(&key) {
-                Some(i)
-            } else {
-                hist.insert(key, j)
-            }
-
-    });
-
+    println!("Start:");
+    map.dump('.');
+    map = repeat(map,
+                 |mut map| {
+                    roll(&mut map, &Direction::North);
+                    roll(&mut map, &Direction::West);
+                    roll(&mut map, &Direction::South);
+                    roll(&mut map, &Direction::East);
+                    println!("\nEast *:");
+                    map.dump('.');
+                    map
+                 },
+                 map_state_key,
+                 1000000000
+                 );
     load(&map)
 }
 
 #[cfg(test)]
 mod tests {
     use super::*;
+
+    #[test]
+    fn repeat_test() {
+        let state = "abcabcabc".as_bytes();
+        let a = repeat(state, |s| &s[1..], |s| s[0], 7);
+        assert_eq!(a[0], state[7]);
+    }
 
     #[test]
     fn part1_test() {
@@ -198,6 +245,6 @@ mod tests {
     #[test]
     fn part2_final_test() {
         let input = include_str!("../input.txt");
-        assert_eq!(part2(input), 1);
+        assert_eq!(part2(input), 93742);
     }
 }

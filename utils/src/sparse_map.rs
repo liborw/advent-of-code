7941adc82ea9105943db1c4f70e9e6fcc3aca35d
Libor Wagner (ohj8Ze) <libor.wagner@cvut.cz>
2024-12-04 09:17:55 +0100

    Utils update

--- utils/src/sparse_map.rs
+++ utils/src/sparse_map.rs
@@ -29,12 +29,69 @@ impl Bounds {
     pub fn zero() -> Self {
         Bounds{x_min: 0, y_min: 0, x_max: 0, y_max: 0}
     }
+
+    pub fn offset(&mut self, offset: i32) {
+        self.x_max += offset;
+        self.y_max += offset;
+        self.x_min -= offset;
+        self.y_min -= offset;
+    }
+
 }
 
 pub trait Map<T> {
     fn bounds(&self) -> Bounds;
-    fn print(&self, bg: T);
     fn print_with_bounds(&self, bg: T, bounds: &Bounds);
+
+    fn print(&self, bg: T) {
+        self.print_with_bounds(bg, &self.bounds());
+    }
+}
+
+pub fn n_digits(n: i32) -> usize {
+    n.to_string()
+        .chars()
+        .count()
+}
+
+pub fn digits(n: i32) -> Vec<char> {
+    n.to_string()
+        .chars()
+        .collect()
+}
+
+pub fn x_ticks(from: i32, to: i32, padding: usize) -> String {
+
+    let numbers: Vec<String> = (from..=to).map(|v| v.abs().to_string()).collect();
+    let n_digits = numbers.iter().map(|v| v.len()).max().unwrap();
+
+    let mut output = String::new();
+
+    for l in 0..n_digits {
+        // add padding
+        output.push_str(&" ".repeat(padding));
+
+        let i = n_digits - l - 1;
+        for number in numbers.iter() {
+            output.push( number.chars().rev().nth(i).unwrap_or(' '));
+        }
+        output.push('\n');
+    }
+    output
+}
+
+pub fn y_ticks(from: i32, to:i32) -> Vec<String> {
+
+    let mut numbers: Vec<String> = (from..=to).map(|v| v.abs().to_string()).collect();
+    let n_digits = numbers.iter().map(|v| v.len()).max().unwrap();
+
+    // padding
+    for number in numbers.iter_mut() {
+        let pad = n_digits - number.len();
+        *number = " ".repeat(pad) + number;
+    }
+
+    numbers
 }
 
 
@@ -53,22 +110,60 @@ impl<T: Display> Map<T> for HashMap<Pos, T> {
         b
     }
 
-    fn print(&self, bg: T) {
-        let b = self.bounds();
-        for r in b.x_min..=b.x_max  {
-            for c in b.y_min..=b.y_max {
-                print!("{}", self.get(&Pos::new(r, c)).unwrap_or(&bg));
+    fn print_with_bounds(&self, bg: T, bounds: &Bounds) {
+        let mut bounds = bounds.clone();
+        bounds.offset(10);
+
+        let y_ticks = y_ticks(bounds.y_min, bounds.y_max);
+        print!("{}", x_ticks(bounds.x_min, bounds.x_max, y_ticks[0].len()));
+
+        for r in bounds.y_min..=bounds.y_max  {
+            print!("{}", y_ticks[(r - bounds.y_min) as usize]);
+            for c in bounds.x_min..=bounds.x_max {
+                print!("{}", self.get(&Pos::new(c, r)).unwrap_or(&bg));
             }
             println!();
         }
+    }
+}
+
+
+#[cfg(test)]
+mod test {
+    use core::panic;
+
+    use super::*;
+
+    #[test]
+    fn digits_test() {
+        assert_eq!(digits(1), vec!['1']);
+        assert_eq!(digits(21), vec!['2', '1']);
     }
 
-    fn print_with_bounds(&self, bg: T, bounds: &Bounds) {
-        for r in bounds.x_min..=bounds.x_max  {
-            for c in bounds.y_min..=bounds.y_max {
-                print!("{}", self.get(&Pos::new(r, c)).unwrap_or(&bg));
-            }
-            println!();
-        }
+    #[test]
+    fn n_digits_test() {
+        assert_eq!(n_digits(1), 1);
+        assert_eq!(n_digits(92513), 5);
     }
+
+    #[test]
+    fn x_ticks_test() {
+        assert_eq!(x_ticks(0, 3, 0), "0123\n".to_string());
+        assert_eq!(x_ticks(0, 3, 2), "  0123\n".to_string());
+        assert_eq!(x_ticks(-1, 1, 0), "101\n".to_string());
+        assert_eq!(x_ticks(8, 11, 0), "  11\n8901\n".to_string());
+    }
+
+    #[test]
+    fn print_test() {
+        let mut map = HashMap::new();
+        map.insert(Pos::new(0, 0), 'o');
+        map.insert(Pos::new(2, -2), 'o');
+        map.insert(Pos::new(12, 12), 'x');
+
+        map.print('.');
+        panic!();
+    }
+
+
 }

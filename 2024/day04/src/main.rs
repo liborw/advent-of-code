00e587922aade6fa6a29740169443cfa544cc788
Libor Wagner (wagnelib) <libor.wagner@cvut.cz>
2024-12-04 10:40:09 +0100

    Day4

--- 2024/day04/src/main.rs
+++ 2024/day04/src/main.rs
@@ -51,8 +51,32 @@ fn part1(input: &str) -> usize {
 
 }
 
+fn is_x_mas(p: &Pos, map: &HashMap<Pos, char>) -> bool {
+
+    let tl = map.get(&(*p + Pos::new(-1, 1))).unwrap_or(&'-');
+    let tr = map.get(&(*p + Pos::new(1, 1))).unwrap_or(&'-');
+    let bl = map.get(&(*p + Pos::new(-1, -1))).unwrap_or(&'-');
+    let br = map.get(&(*p + Pos::new(1, -1))).unwrap_or(&'-');
+
+    ((*tl, *br) == ('M', 'S') || (*tl, *br) == ('S', 'M')) && ((*tr, *bl) == ('M', 'S') || (*tr, *bl) == ('S', 'M'))
+}
+
 fn part2(input: &str) -> usize {
-    1
+    let map = parse(input);
+
+    // find all X
+    let xs: Vec<Pos> = map.iter()
+        .filter_map(|(&k, &v)| {
+            if v == 'A' {
+                Some(k)
+            } else {
+                None
+            }})
+        .collect();
+
+    xs.into_iter()
+      .filter(|p| is_x_mas(p, &map))
+      .count()
 }
 
 #[cfg(test)]
@@ -74,7 +98,7 @@ mod tests {
     #[test]
     fn part2_test() {
         let input = include_str!("../input_test.txt");
-        assert_eq!(part2(input), 1);
+        assert_eq!(part2(input), 9);
     }
 
     #[test]

e5df217d5fbc22f86026daa3cf364e04447f2438
Libor Wagner (wagnelib) <libor.wagner@cvut.cz>
2024-12-06 15:18:59 +0100

    WIP: day6

--- 2024/day06/src/main.rs
+++ 2024/day06/src/main.rs
@@ -1,4 +1,4 @@
-use std::collections::{HashMap, HashSet};
+use std::{collections::{HashMap, HashSet}};
 
 use utils::{aoc_task, map::{Direction, Map, Pos, SparseMap}, took};
 
@@ -8,107 +8,172 @@ fn main() {
     aoc_task!(|| part2(input));
 }
 
-type Guard = (Pos, Direction);
-type Path = HashSet<(Pos, Direction)>;
-type Cycles = HashMap<(Pos, Direction), bool>;
+#[derive(Clone, PartialEq, Eq, Hash)]
+struct Guard {
+    pos: Pos,
+    dir: Direction
+}
+
+impl Guard {
+    fn new(pos: Pos, dir: Direction) -> Self {
+        Guard{pos, dir}
+    }
+
+    fn turn(&mut self) {
+        self.dir = self.dir.turn_right_90();
+    }
+
+    fn r#move(&mut self) {
+        self.pos = self.pos.r#move(&self.dir, 1);
+    }
+
+    fn next_pos(&self) -> Pos {
+        self.pos.r#move(&self.dir, 1)
+    }
+}
+
+type Path = HashSet<Guard>;
+
 
 fn walk(guard: &Guard, map: &SparseMap<char>) -> Path {
-    let mut pos = guard.0;
-    let mut dir = guard.1;
+    let mut g = guard.clone();
     let mut path = Path::new();
 
-    while map.get(&pos).is_some() {
-        path.insert((pos, dir));
-        let mut new_pos = pos.r#move(&dir, 1);
-        while map.get(&new_pos) == Some(&'#') {
-            dir = dir.turn_right_90();
-            new_pos = pos.r#move(&dir, 1);
+    while map.get(&g.pos).is_some() {
+        path.insert(g.clone());
+
+        // turn if needed
+        while map.get(&g.next_pos()) == Some(&'#') {
+            g.turn()
         }
-        pos = new_pos;
+        path.insert(g.clone());
+        g.r#move();
     }
     path
 }
 
-
-
 fn get_guard(map: &SparseMap<char>) -> Guard {
     let pos = map.find_all(&|c| "<>^v".chars().any(|v| v == *c)).next().unwrap();
     let dir = map.get(&pos).unwrap().try_into().unwrap();
-    (pos, dir)
+    Guard::new(pos, dir)
 }
 
 
 fn part1(input: &str) -> usize {
     let map = SparseMap::from_str(input, &|v| Some(v));
     let guard = get_guard(&map);
-    walk(&guard, &map).into_iter().map(|v| v.0).collect::<HashSet<Pos>>().len()
+    walk(&guard, &map).into_iter().map(|g| g.pos).collect::<HashSet<Pos>>().len()
 }
 
-fn walk_with_cycles(pos: Pos, dir: Direction, cycles: &Cycles, map: &SparseMap<char>) -> (Path, bool) {
-    let mut pos = pos;
-    let mut dir = dir;
-    let mut path = Path::new();
+fn walk_with_cycle(guard: &Guard, path: &Path, map: &SparseMap<char>) -> bool {
+    let mut g = guard.clone();
+    let mut path = path.clone();
 
-    while map.get(&pos).is_some() {
+    while map.get(&g.pos).is_some() {
 
-        match (cycles.get(&(pos, dir)), path.contains(&(pos, dir))) {
-            (Some(v), _) => return (path, *v),
-            (None, true) => return (path, true),
-            _ => ()
+        if path.contains(&g) {
+            return true
         }
-        path.insert((pos, dir));
+        path.insert(g.clone());
 
-        let mut new_pos = pos.r#move(&dir, 1);
-        while map.get(&new_pos) == Some(&'#') {
-            dir = dir.turn_right_90();
-            new_pos = pos.r#move(&dir, 1);
+        // turn if needed
+        while map.get(&g.next_pos()) == Some(&'#') {
+            g.turn()
         }
-        pos = new_pos;
+        path.insert(g.clone());
+        g.r#move();
     }
-    (path, false)
+    false
 }
 
-fn find_obstacles(guard: &Guard, map: &mut SparseMap<char>) -> Vec<Pos> {
-    let mut pos = guard.0;
-    let mut dir = guard.1;
-    let mut cycles = Cycles::new();
+fn find_obstacles(guard: &Guard, map: &SparseMap<char>) -> Vec<Pos> {
+    let mut g = guard.clone();
+    let mut path = Path::new();
     let mut obstacles = HashSet::new();
+    let mut map = map.clone();
 
-    while map.get(&pos).is_some() {
-        let mut new_pos = pos.r#move(&dir, 1);
+    while map.contains_key(&g.pos) {
+        let test_pos = g.next_pos();
 
         // try to add obstacle
-        let dir_test = dir.turn_right_90();
-        if map.get(&new_pos) == Some(&'.') {
-            map.insert(new_pos, '#');
-            let (path, is_cycle) = walk_with_cycles(pos, dir_test, &cycles, map);
-            map.insert(new_pos, '.');
-
-            if is_cycle {
-                //path.into_iter().for_each(|k| {cycles.insert(k, is_cycle);});
-                obstacles.insert(new_pos);
+        if map.get(&test_pos) == Some(&'.') {
+            map.insert(test_pos, '#');
+            if walk_with_cycle(&g, &path, &map) {
+                obstacles.insert(test_pos);
             }
+            map.insert(test_pos, '.');
         }
 
-        cycles.insert((pos, dir), true);
-        while map.get(&new_pos) == Some(&'#') {
-            dir = dir.turn_right_90();
-            new_pos = pos.r#move(&dir, 1);
+        path.insert(g.clone());
+
+        // turn if needed
+        while map.get(&g.next_pos()) == Some(&'#') {
+            g.turn()
         }
-        pos = new_pos;
+        path.insert(g.clone());
+        g.r#move();
     }
     obstacles.into_iter().collect()
 }
 
-fn part2(input: &str) -> usize {
-    let mut map = SparseMap::from_str(input, &|v| Some(v));
-    let guard = get_guard(&map);
-    let obstacles = find_obstacles(&guard, &mut map);
-    map.print('.');
-    println!();
+fn draw_cyrcles(guard: &Guard, map: &SparseMap<char>) -> SparseMap<char> {
+    let mut g = guard.clone();
+    let mut path = Path::new();
+    let mut map = map.clone();
 
-    obstacles.iter().for_each(|p| {map.insert(*p, 'O');});
-    map.print('.');
+    while map.contains_key(&g.pos) {
+
+        if path.contains(&g) {
+            map.insert(g.pos, '+');
+            break
+        }
+
+        path.insert(g.clone());
+
+        // turn if needed
+        let mut new_g = g.clone();
+        while map.get(&new_g.next_pos()) == Some(&'#') {
+            new_g.turn()
+        }
+
+        path.insert(new_g.clone());
+        new_g.r#move();
+
+        let label = if new_g.dir != g.dir || map.get(&g.pos) == Some(&'|') || map.get(&g.pos) == Some(&'-') {
+            '+'
+        } else {
+            match g.dir {
+                Direction::North | Direction::South => '|',
+                Direction::West | Direction::East => '-',
+                _ => ' '
+            }
+        };
+        map.insert(g.pos, label);
+
+        g = new_g;
+
+    }
+    map
+
+}
+
+fn part2(input: &str) -> usize {
+    let map = SparseMap::from_str(input, &|v| Some(v));
+    let guard = get_guard(&map);
+    let obstacles = find_obstacles(&guard, &map);
+
+    // debug
+
+    // for o in obstacles.iter() {
+    //     let mut map = map.clone();
+    //     map.insert(*o, '#');
+    //     map = draw_cyrcles(&guard, &map);
+    //     map.insert(*o, 'O');
+    //     map.insert(guard.pos, '^');
+    //     map.print('X');
+    //     println!();
+    // }
+
     obstacles.len()
 }
 
@@ -132,6 +197,12 @@ mod tests {
     fn part2_test() {
         let input = include_str!("../input_test.txt");
         assert_eq!(part2(input), 6);
+    }
+
+    #[test]
+    fn part2_test2() {
+        let input = include_str!("../input_test2.txt");
+        assert_eq!(part2(input), 1);
     }
 
     #[test]

0a35af5ba3ef07b75ff69cf0a33e2590131c6723
Libor Wagner (ohj8Ze) <libor.wagner@cvut.cz>
2024-12-08 22:00:50 +0100

    Improved aoc template

--- templates/aoc/src/main.rs
+++ templates/aoc/src/main.rs
@@ -1,19 +1,17 @@
-use common::{took, aoc_task};
-
-
+use utils::{took, run_task};
 
 fn main() {
     let input = include_str!("../input.txt");
-    aoc_task!(|| part1(input));
-    aoc_task!(|| part2(input));
+    run_task!(|| part1(input));
+    run_task!(|| part2(input));
 }
 
 fn part1(input: &str) -> usize {
-    1
+    0
 }
 
 fn part2(input: &str) -> usize {
-    1
+    0
 }
 
 #[cfg(test)]
@@ -21,25 +19,25 @@ mod tests {
     use super::*;
 
     #[test]
-    fn part1_test() {
+    fn {{ project-name }}_part1_test() {
         let input = include_str!("../input_test.txt");
         assert_eq!(part1(input), 1);
     }
 
     #[test]
-    fn part1_final_test() {
+    fn {{ project-name }}_part1_final_test() {
         let input = include_str!("../input.txt");
         assert_eq!(part1(input), 1);
     }
 
     #[test]
-    fn part2_test() {
+    fn {{ project-name }}_part2_test() {
         let input = include_str!("../input_test.txt");
         assert_eq!(part2(input), 1);
     }
 
     #[test]
-    fn part2_final_test() {
+    fn {{ project-name }}_part2_final_test() {
         let input = include_str!("../input.txt");
         assert_eq!(part2(input), 1);
     }

338a9fcaaf58d35bee17f4f3a0eb62df0498fe6c
Libor Wagner (ohj8Ze) <libor.wagner@cvut.cz>
2024-12-12 20:28:44 +0100

    WIP: Day13

---
+++ 2024_aoc/day13/src/main.rs
@@ -0,0 +1,44 @@
+use utils::{took, run_task};
+
+fn main() {
+    let input = include_str!("../input.txt");
+    run_task!(|| part1(input));
+    run_task!(|| part2(input));
+}
+
+fn part1(input: &str) -> usize {
+    0
+}
+
+fn part2(input: &str) -> usize {
+    0
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn day13_part1_test() {
+        let input = include_str!("../input_test.txt");
+        assert_eq!(part1(input), 1);
+    }
+
+    #[test]
+    fn day13_part1_final_test() {
+        let input = include_str!("../input.txt");
+        assert_eq!(part1(input), 1);
+    }
+
+    #[test]
+    fn day13_part2_test() {
+        let input = include_str!("../input_test.txt");
+        assert_eq!(part2(input), 1);
+    }
+
+    #[test]
+    fn day13_part2_final_test() {
+        let input = include_str!("../input.txt");
+        assert_eq!(part2(input), 1);
+    }
+}
